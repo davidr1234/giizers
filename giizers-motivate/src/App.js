2c3a8695793b05dc8db5bf3198bd2ba9d1f1a748
davidr85 <32004282+davidr85@users.noreply.github.com>
2021-09-25 19:09:24 +0200

    barcode scanner

--- giizers-motivate/src/App.js
+++ giizers-motivate/src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import React from 'react';
 
 function App() {
+
+  const [data, setData] = React.useState("Not Found");
+
+  const imageLoader = () => {
+    return (
+      <div>
+        <img src="https://image.migros.ch/2017-large/6fa8aec688e81a86612d4208aa87da936f408991/focus-vitamin-water-active.jpg"/>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +31,19 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(imageLoader);
+          else setData(<div style={{textAlign: 'center'}}>Bitte platzieren Sie den Strichcode vor die Kamera</div>);
+        }}
+      />
+
+      <p>{data}</p>
+
+      </body>
     </div>
   );
 }
